caf86982b4fa7cc87657c916e87170bb26ee5520
magrowing <kmagrowing@gmail.com>
2024-06-02 18:24:48 +0900

    Day06 : 과제 Sing page

--- Day06/homework-react-app/src/App.tsx
+++ Day06/homework-react-app/src/App.tsx
@@ -1,9 +1,11 @@
 import Login from './pages/Login';
+import Sign from './pages/Sign';
 
 export default function App() {
   return (
-    <article className="flex items-center justify-center h-[100vh] rounded-md">
+    <article className="flex items-center justify-center h-[100vh]">
       <Login />
+      <Sign />
     </article>
   );
 }
---
+++ Day06/homework-react-app/src/pages/Sign.tsx
@@ -0,0 +1,28 @@
+import Title from '../components/Title';
+import TextField from '../components/TextField';
+import CheckBox from '../components/CheckBox';
+import Button from '../components/Button';
+
+export default function Sign() {
+  return (
+    <section className="section-wrapper">
+      <div>
+        <Title type="main">Sign Into App</Title>
+        <Title type="sub">Please enter your details to continue.</Title>
+      </div>
+      <form className="form-wrapper">
+        <TextField type="text" placeholder="Enter Your Name" />
+        <TextField type="text" placeholder="someone@example.com" />
+        <TextField type="password" placeholder="Enter Password" />
+        <CheckBox>
+          I agree with <span className="bold">terms</span> and{' '}
+          <span className="bold">policies.</span>
+        </CheckBox>
+      </form>
+      <div className="btn-wrapper">
+        <Button btnStyleType="full">Sign In</Button>
+        <Button btnStyleType="line">Go To Log In</Button>
+      </div>
+    </section>
+  );
+}
