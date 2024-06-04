d8e8b4f2271f4a54aa890cde21fc6cd48ce3d829
magrowing <kmagrowing@gmail.com>
2024-06-04 23:44:03 +0900

    Day08 : TodoList 최적화

--- Day08/src/components/TodoListItem.tsx
+++ Day08/src/components/TodoListItem.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+/* eslint-disable react-refresh/only-export-components */
+import { useState, memo } from 'react';
 
 import Checkbox from './html/Checkbox';
 import Input from './html/Input';
@@ -69,7 +70,7 @@ const TodoListItem = (props: TodoListItemProps) => {
             fill="currentColor"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M11.013 2.513a1.75 1.75 0 0 1 2.475 2.474L6.226 12.25a2.751 2.751 0 0 1-.892.596l-2.047.848a.75.75 0 0 1-.98-.98l.848-2.047a2.75 2.75 0 0 1 .596-.892l7.262-7.261Z"
               clipRule="evenodd"
             />
@@ -102,4 +103,4 @@ const TodoListItem = (props: TodoListItemProps) => {
     </>
   );
 };
-export default TodoListItem;
+export default memo(TodoListItem);
