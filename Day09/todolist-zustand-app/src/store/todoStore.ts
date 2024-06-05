fe0f3123d7d15d274d3df67040370a39a5a5eb70
magrowing <kmagrowing@gmail.com>
2024-06-05 23:19:23 +0900

    Day09 : 과제 store action 객체 생성

--- Day09/todolist-zustand-app/src/store/todoStore.ts
+++ Day09/todolist-zustand-app/src/store/todoStore.ts
@@ -14,43 +14,47 @@ import {
 
 type TodoStoreType = {
   todos: Todo[];
-  getTodos: () => void;
-  addTodo: (task: string) => void;
-  toggleTodo: (id: string) => void;
-  removeTodo: (id: string) => void;
-  updateTodo: (id: string, task: string) => void;
+  action: {
+    getTodos: () => void;
+    addTodo: (task: string) => void;
+    toggleTodo: (id: string) => void;
+    removeTodo: (id: string) => void;
+    updateTodo: (id: string, task: string) => void;
+  };
 };
 
 export const useTodoStore = create<TodoStoreType>((set) => ({
   todos: [],
-  getTodos: async () => {
-    const data = await getTodosData();
-    set(() => ({ todos: data }));
-  },
-  addTodo: async (task) => {
-    const data = await postTodoData(task);
-    set((state) => ({ todos: [data, ...state.todos] }));
-  },
-  toggleTodo: async (id) => {
-    await patchTodoData(id);
-    set((state) => ({
-      todos: state.todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      ),
-    }));
-  },
-  removeTodo: async (id) => {
-    await deleteTodoData(id);
-    set((state) => ({
-      todos: state.todos.filter((todo) => todo.id !== id),
-    }));
-  },
-  updateTodo: async (id, task) => {
-    await putTodoData(id, task);
-    set((state) => ({
-      todos: state.todos.map((todo) =>
-        todo.id === id ? { ...todo, task } : todo
-      ),
-    }));
+  action: {
+    getTodos: async () => {
+      const data = await getTodosData();
+      set(() => ({ todos: data }));
+    },
+    addTodo: async (task) => {
+      const data = await postTodoData(task);
+      set((state) => ({ todos: [data, ...state.todos] }));
+    },
+    toggleTodo: async (id) => {
+      await patchTodoData(id);
+      set((state) => ({
+        todos: state.todos.map((todo) =>
+          todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        ),
+      }));
+    },
+    removeTodo: async (id) => {
+      await deleteTodoData(id);
+      set((state) => ({
+        todos: state.todos.filter((todo) => todo.id !== id),
+      }));
+    },
+    updateTodo: async (id, task) => {
+      await putTodoData(id, task);
+      set((state) => ({
+        todos: state.todos.map((todo) =>
+          todo.id === id ? { ...todo, task } : todo
+        ),
+      }));
+    },
   },
 }));
